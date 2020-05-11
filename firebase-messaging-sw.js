c3c87eb1c47fce5e7558c5e490c38dd40f11c7d1
Raghu Thakur <44355846+raghuthakur@users.noreply.github.com>
2020-05-11 09:16:53 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/4.9.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.9.1/firebase-messaging.js');
 /*Update this config*/
 var config = {
-    apiKey: "AIzaSyCZzw4RTesKrY3EqqPHSZ080EwZri9hUr0",
-    authDomain: "sterkla-8961c.firebaseapp.com",
-    databaseURL: "https://sterkla-8961c.firebaseio.com",
-    projectId: "sterkla-8961c",
-    storageBucket: "sterkla-8961c.appspot.com",
-    messagingSenderId: "452039076434",
-    appId: "1:452039076434:web:17819d0acd2a73c0"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   firebase.initializeApp(config);
 
